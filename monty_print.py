1871324c311836adb62175fd4b05c76ee539e316
camkirk <camden.kirkland@gmail.com>
2019-03-16 04:19:02 -0500

    prints wins by prior and posterior

--- monty_print.py
+++ monty_print.py
@@ -13,6 +13,10 @@ def print_values(results):
     df = pd.DataFrame(results)
     df = df.rename(columns={0:"Prior", 1:"Posterior",2:"Result" })
 
+    # abstract groupby calls
+
+    prior_group = df.groupby("Prior")
+    posterior_group = df.groupby(["Prior", "Posterior"])
 
 
 
@@ -21,18 +25,20 @@ def print_values(results):
     print("""
     Distribution of Prior Selections p(A)
     """)
-    print(df.groupby("Prior").count()["Result"])
+    print(prior_group.count()["Result"])
 
     print("""
     Distribution of Posterior Selections 
     Note, this is NOT the distribution of WINNING,
     only the rate at which each value is selected
     """)
-    print(df.groupby(["Prior", "Posterior"]).count()["Result"])
+    print(posterior_group.count()["Result"])
 
 
 
     # Print Wins, Losses, and Winning Rate
+
+    print("Data on wins")
 
     winrate_series = df.groupby(["Result"]).count()["Posterior"]
 
@@ -41,4 +47,27 @@ def print_values(results):
         Losses: {winrate_series[0]} 
         Winrate: {winrate_series[1]/(winrate_series[1]+winrate_series[0])}
         """)
-    # print(df)
+
+    # Wins by Prior
+    
+    print("""
+    Wins by Prior Selection
+    """)
+
+    prior_win_df = prior_group.count().join(prior_group.sum(), rsuffix="win")
+    prior_win_df.rename(inplace=True, columns={"Result":"Trials", "Resultwin":"Wins"})
+    prior_win_df = prior_win_df[["Trials","Wins"]]
+    prior_win_df["Winrate"] = prior_win_df["Wins"]/prior_win_df["Trials"]
+    print(prior_win_df)
+
+    # Wins by Posterior
+
+    print("""
+    Wins by Posterior Selection
+    """)
+
+    posterior_win_df = posterior_group.count().join(posterior_group.sum(), rsuffix="win")
+    posterior_win_df.rename(inplace=True, columns={"Result":"Trials", "Resultwin":"Wins"})
+    posterior_win_df = posterior_win_df[["Trials","Wins"]]
+    posterior_win_df["Winrate"] = posterior_win_df["Wins"]/posterior_win_df["Trials"]
+    print(posterior_win_df)
